f169af65ddbd6ace0dc4303f735b1ca5ea588c89
Pede <Pede@w66242.web1.wireless.dtu.dk>
2015-05-06 14:06:01 +0200

    LogoutUI

--- src/opr/client/ui/SetInactiveUI.java
+++ src/opr/client/ui/SetInactiveUI.java
@@ -1,13 +1,13 @@
 package opr.client.ui;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
-import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.uibinder.client.UiHandler;
-import com.google.gwt.event.dom.client.ClickEvent;
 
 public class SetInactiveUI extends Composite{
 
